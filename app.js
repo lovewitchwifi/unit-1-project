3564576d1b421b87fef307f1f8323aa3b9f62b64
lovewitchwifi <mel.blanco.mail@gmail.com>
2023-05-06 17:33:42 -0500

    a little prettier css

--- app.js
+++ app.js
@@ -1,59 +1,66 @@
+// Set categories to "song lyrics", "locations", "girl names", "puerto rican slang", and "wild card"
+
 const categories = ['song lyrics', 'locations', 'girl names', 'puerto rican slang', 'wild card']
 
+// Set questions and answers within each category:
+
 const songLyricsQuestions = 
-      ["What is the messaging app Bad Bunny references when he laments the feeling of someone not saving you as a contact in their phone: ‘____ sin el retrato, no guarda mi contacto’?", "Question two", "Question three"]
+    ["What is the messaging app Bad Bunny references when he laments the feeling of someone not saving you as a contact in their phone: ‘____ sin el retrato, no guarda mi contacto’?", 
+    "Finish this line in the song “Me Porto Bonito”: 'Tu no eres bebesita, __ ____ ________'", 
+    "In the song, “Yo no soy celoso”, Bad Bunny is stating that he’s not the jealous type but seeing his girl with another guy still begs what question?"]
 
 const songLyricsAnswers =
-      ["Answer 1", "Answer 2", "Answer 3"]
+    ["What is WhatsApp?", 
+    "What is tu eres bebesota?", 
+    "What is pero quien es ese cabron?"]
+
+const locationsQuestions = 
+    ["In the song “Ensename a bailar” Bad Bunny refers to the island Quisqeya, who colonized this island in 1492?", 
+    "In the song 'Andrea', Bad Bunny tells a story about Andrea leaving what popular nightlife area at 4 in the morning?", 
+    "What is the secret beach town where Bad Bunny recorded ‘Un Verano Sin Ti’?"]
+
+const locationsAnswers =
+    ["Who is Christopher Columbus?", 
+    "What is La Perla?", 
+    "What is Aniaco?"]
+
+const prSlangQuestions =
+    ["What is the puerto rican term for dancing or rather, twerking?",
+    "Bad Bunny’s hit single “TITI ME PREGUNTO” climbed the charts in 2022, who is he referring to when he addresses “TITI”?",
+    "When Bad Bunny says “Puerto Rico está bien cabrón, ey, está bien cabrón”, he is using the term “cabron” which can often mean what?"]
+
+const prSlangAnswers =
+    ["What is perreo?",
+    "What is an aunty?",
+    "What is cool?"]
+
+const girlNamesQuestions =
+    ["Out of all the names mentioned in the hit song “Titi Me Pregunto”, what is the name of the one woman confirmed Bad Bunny has had a serious relationship with in the past?",
+    "Who does Bad Bunny name as being his first girlfriend in kindergarten when he says, “Mi primera novia en kinder, _____”?",
+    "At almost 6 minutes long, this song sends a powerful message on femicide. The track sheds light on the case of a Puerto Rican woman murdered by her abusive ex-boyfriend after the courts failed to protect her. What is this woman’s name?"]
+
+const girlNamesAnswers =
+    ["Who is Gabriela?",
+    "Who is Maria?",
+    "Who is Andrea?"]
+
+const wildCardQuestions =
+    ["“Despues de la playa” has a distinctive sound compared to the rest of the album, what genre is Bad Bunny paying homage to in this song?",
+    "In “El Apagon” Bad Bunny is paying homage to his beloved home, Puerto Rico, by highlighting the effects of power blackouts throughout the territory. What energy company is widely known as being responsible for these blackouts?",
+    "In the music video for 'Neverita' Bad Bunny paid homage to a famous latin song and singer by replicating the exact same music video but starring himself. What is the name of the song and artist that Benito is paying homage to?"]
+
+const wildCardAnswers =
+    ["What is salsa?",
+    "What is Luma Energy?",
+    "What is Suavemente by Elvis Crespo?"]
+
+const songLyricsQuestion100 = document.querySelector("#song-lyrics-100")
+
+songLyricsQuestion100.addEventListener('click', (event){
+    console.log('test');
+})
 
 
-const sLQ1 = documnet.querySelector("child song-lyrics-100")
-
-sLQ1.addEventListener("click", function(){})
-      
-      
-  //     {
-  //   category: "song lyrics",
-  //   questions: [
-  //     {
-  //         question-one: "What is the messaging app Bad Bunny references when he laments the feeling of someone not saving you as a contact in their phone: ‘____ sin el retrato, no guarda mi contacto’?",   
-  //         answer-one: ["WHATSAPP"],
-  //         level: "easy",
-  // }
-  // }
-  
-
-
-
-
-// 
-// Set categories to "song lyrics", "locations", "girl names", "puerto rican slang", and "wild card"
-// Set questions within each category:
-
-// Song lyrics category will have [array]
-// [question 1 answer 1]
-// [question 2 answer 2]
-// [question 3 answer 3]
-
-// Locations category will have [array]
-// [question 1 answer 1]
-// [question 2 answer 2]
-// [question 3 answer 3]
-
-// Girl names category will have [array]
-// [question 1 answer 1]
-// [question 2 answer 2]
-// [question 3 answer 3]
-
-// Puerto rican slang category will have [array]
-// [question 1 answer 1]
-// [question 2 answer 2]
-// [question 3 answer 3]
-
-// Wild card category will have [array]
-// [question 1 answer 1]
-// [question 2 answer 2]
-// [question 3 answer 3]
 
 // Then we will have functions to display the question and answer
 
